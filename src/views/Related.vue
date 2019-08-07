809384280e3cfa9e3dab86808bd39b8acab1c337
Jon Long <jon.long@voxelcreative.ca>
2019-08-07 03:29:26 -0700

    Related working

--- src/views/Related.vue
+++ src/views/Related.vue
@@ -10,7 +10,14 @@
             <Search :hrefFunc="hrefFunc" />
           </div>
           <div v-if="arg">
-            
+            <WordListExended v-if="word" :words="[word]" />
+            <h4 class="text-center mt-5 mb-5">Words of Similar Meaning</h4>
+            <Merge direction="bottom" class="h-half mt-5 mb-5" />
+            <WordListExended v-if="similar.length > 0" :words="similar" />
+            <Merge direction="top" class="h-half mt-5 mb-5" />
+            <h4 class="text-center">Related Words</h4>
+            <Merge direction="bottom" class="h-half mt-5 mb-5" />
+            <WordListExended v-if="related.length > 0" :words="related" />
           </div>
         </div>
       </div>
@@ -23,12 +30,15 @@ import Helper from '@/lib/helper'
 import WordListExended from '@/components/WordListExended.vue'
 import DefinitionsList from '@/components/DefinitionsList.vue'
 import Search from '@/components/Search.vue'
+import SketchEngine from '@/lib/sketch-engine'
+import Merge from '@/components/Merge'
 import $ from 'jquery'
 
 export default {
   components: {
     WordListExended,
     DefinitionsList,
+    Merge,
     Search
   },
   beforeMount() {
@@ -39,11 +49,51 @@ export default {
       $('#hsk-dictionary')[0].scrollIntoView()
       if (this.$route.params.arg) {
         this.arg = this.$route.params.arg
+        Helper.loaded(
+          (LoadedAnnotator, LoadedHSKCEDICT, loadedGrammar, LoadedHanzi) => {
+            LoadedHSKCEDICT.getByIdentifier(
+              word => {
+                this.word = word
+                for (let definition of this.word.definitions) {
+                  LoadedHSKCEDICT.lookupByDefinition(
+                    words => {
+                      for (let word of words.filter(
+                        word => word.hsk !== 'outside'
+                      )) {
+                        if (word.identifier !== this.word.identifier) {
+                          this.similar.push(word)
+                        }
+                      }
+                    },
+                    [definition.text]
+                  )
+                }
+                SketchEngine.thesaurus(this.word.simplified, response => {
+                  this.words = []
+                  if (response) {
+                    for (let Word of response.Words) {
+                      Helper.loaded((LoadedAnnotator, LoadedHSKCEDICT) => {
+                        LoadedHSKCEDICT.lookupSimplified(
+                          words => {
+                            words = words.filter(word => word.hsk !== 'outside')
+                            if (words.length > 0) {
+                              let word = words[0]
+                              this.related.push(word)
+                            }
+                          },
+                          [Word.word]
+                        )
+                      })
+                    }
+                  }
+                })
+              },
+              [this.arg]
+            )
+          }
+        )
       } else {
         this.arg = ''
-        Helper.loaded(
-          (LoadedAnnotator, LoadedHSKCEDICT, loadedGrammar, LoadedHanzi) => {}
-        )
       }
     }
   },
@@ -55,8 +105,10 @@ export default {
   data() {
     return {
       Helper,
-      root: undefined,
+      word: undefined,
       arg: undefined,
+      similar: [],
+      related: [],
       hrefFunc: entry => `#/explore/related/${entry.identifier}`
     }
   }
