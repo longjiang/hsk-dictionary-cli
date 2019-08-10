297e12a1bf38dee32ae51023a9d23ed26515b75d
Jon Long <jon.long@voxelcreative.ca>
2019-08-10 15:55:40 -0700

    Finish styling instagram

--- src/components/DefinitionsList.vue
+++ src/components/DefinitionsList.vue
@@ -2,15 +2,14 @@
   <div>
     <div v-if="definitions && definitions.length > 0">
       <ul class="definitions collapsed mb-2" data-collapse-target>
-        <li v-for="definition in definitions" class="english">
-          {{ definition.text ? definition.text : definition}}
-        </li>
+        <li
+          v-for="definition in definitions"
+          class="english"
+        >{{ definition.text ? definition.text : definition }}</li>
       </ul>
       <ShowMoreButton :length="definitions.length" :min="3" class="focus-exclude" />
     </div>
-    <div class="english" v-else>
-      {{ nodef }}
-    </div>
+    <div class="english" v-else>{{ nodef }}</div>
   </div>
 </template>
 
--- src/components/InstagramButton.vue
+++ src/components/InstagramButton.vue
@@ -20,6 +20,23 @@
           <img src="img/instagram-badge.png" class="instagram-badge" />
           <img src="img/logo-mark.png" class="instagram-logo" />
           <EntryHeader :entry="entry" />
+          <div class="example-wrapper pt-4 pb-4">
+            <div class="example-sentence mt-4">
+              <!-- <p class="example-sentence-pinyin">{{ entry.examplePinyin }} <i class="speak glyphicon glyphicon-volume-up" v-bind:data-speak="entry.example"></i></p> -->
+
+              <PinyinButton class="mb-3" />
+              <p
+                class="example-sentence-word"
+                v-html="
+                  Helper.highlight(entry.example, entry.simplified, entry.hsk)
+                "
+              ></p>
+              <hr>
+              <p class="example-sentence-english">
+                {{ entry.exampleTranslation }}
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -28,6 +45,7 @@
 
 <script>
 import $ from 'jquery'
+import Helper from '@/lib/helper'
 import EntryHeader from '@/components/EntryHeader'
 import html2canvas from 'html2canvas'
 
@@ -37,7 +55,9 @@ export default {
     EntryHeader
   },
   data() {
-    return {}
+    return {
+      Helper
+    }
   },
   computed: {
     images() {
@@ -108,6 +128,24 @@ export default {
 
 .instagram-content .word [data-hsk] {
   color: white !important;
+  font-size: 2em;
+}
+
+.instagram-content .pinyin {
+  font-size: 2em;
+}
+
+.instagram-content .definitions li {
+  display: inline;
+}
+
+.instagram-content .definitions li:not(:last-child)::after {
+  content: ', ';
+}
+
+.instagram-content .definitions .english {
+  font-size: 2em;
+  font-weight: 200;
 }
 
 .instagram-content .entry-head-wrapper {
@@ -124,5 +162,34 @@ export default {
   height: 96%;
   left: 2%;
   top: 2%;
+}
+
+.instagram-content .example-wrapper {
+  position: absolute;
+  width: 90%;
+  left: 5%;
+  top: 63%;
+  text-align: center;
+}
+
+.instagram-content .example-sentence-word {
+  font-size: 2em;
+  font-weight: 200;
+}
+
+.instagram-content .example-sentence hr {
+  border: 1px solid rgba(255,255,255,0.5);
+  width: 40%;
+
+}
+
+.example-sentence-english {
+  font-size: 1.5em;
+  font-weight: 200;
+}
+
+.instagram-content .example-sentence-word span[data-hsk] {
+  font-weight: bold;
+  color: white !important;
 }
 </style>
