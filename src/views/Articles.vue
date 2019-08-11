fe91700275102eda3ecb1ca42b89d48af9832f11
Jon Long <jon.long@voxelcreative.ca>
2019-08-11 12:56:31 -0700

    Fix coding error in compare.

---
+++ src/views/Articles.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container main">
+      <div class="ro mt-5">
+        <div class="col-sm-12">
+          <h3>Articles</h3>
+          <hr>
+        </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
