7238b2a46d11b0a5260a1d8ed228f5c7bb769390
Jon Long <jon.long@voxelcreative.ca>
2019-08-13 10:56:25 -0700

    Add Reddit article comments.

--- src/components/RedditArticle.vue
+++ src/components/RedditArticle.vue
@@ -1,16 +1,27 @@
 <template>
   <div class="col-sm-12" v-if="article">
-    <Annotate tag="h3">{{ article.title }}</Annotate>
-    <hr />
-    <div
-      v-if="article.media && article.media.oembed"
-      v-html="Helper.unescape(article.media.oembed.html)"
-    ></div>
-    <img v-if="article.post_hint === 'image'" :src="article.url" alt />
-    <Annotate
-      tag="article"
-      v-html="Helper.unescape(article.selftext_html)"
-    ></Annotate>
+    <article>
+      <Annotate tag="h3">{{ article.title }}</Annotate>
+      <hr />
+      <div
+        v-if="article.media && article.media.oembed"
+        v-html="Helper.unescape(article.media.oembed.html)"
+      ></div>
+      <img v-if="article.post_hint === 'image'" :src="article.url" alt />
+      <Annotate
+        tag="div"
+        v-html="Helper.unescape(article.selftext_html)"
+      ></Annotate>
+    </article>
+    <div>
+      <hr class="mt-5 mb-5">
+      <h4>Comments</h4>
+      <div v-for="comment in comments">
+        <h6>{{ comment.author}} </h6>
+        <Annotate v-html="Helper.unescape(comment.body_html)"></Annotate>
+        <hr>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -35,10 +46,10 @@ export default {
       `${Config.proxy}?https://www.reddit.com/comments/${this.articleId}/.json`,
       response => {
         let article = response.data[0].data.children[0].data
-        console.log(article)
+        // console.log(article)
         this.article = article
         let comments = response.data[1].data.children.map(item => item.data)
-        console.log(comments)
+        // console.log(comments)
         this.comments = comments
       }
     )
