9c3a16eef028fdea068db9db87b7a86058d4c79e
Jon Long <jon.long@voxelcreative.ca>
2019-08-14 08:53:14 -0700

    Reddit article loads gif correctly

--- src/components/RedditArticle.vue
+++ src/components/RedditArticle.vue
@@ -12,6 +12,14 @@
           :src="article.url"
           class="img-fluid"
         />
+        <video
+          v-if="article.post_hint === 'hosted:video'"
+          :src="article.media.reddit_video.fallback_url"
+          class="img-fluid"
+          :autoplay="article.media.reddit_video.is_gif"
+          :loop="article.media.reddit_video.is_gif"
+          preload="auto"
+        ></video>
         <div class="media-body">
           <Annotate tag="h3">{{ article.title }}</Annotate>
           <div
@@ -80,7 +88,7 @@ export default {
       }/.json`,
       response => {
         let article = response.data[0].data.children[0].data
-        // console.log(article, 'article')
+        console.log(article, 'article')
         this.article = article
         let comments = response.data[1].data.children.map(item => item.data)
         // console.log(comments, 'comments')
