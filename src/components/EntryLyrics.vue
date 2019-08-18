70021fc36c233e73cf0a7aab4278fcbaa79fa313
Jon Long <jon.long@voxelcreative.ca>
2019-08-18 16:36:38 -0400

    Separate <Song> into its own component.

--- src/components/EntryLyrics.vue
+++ src/components/EntryLyrics.vue
@@ -7,108 +7,24 @@
       v-cloak
     >
       <div class="container">
-          <div class="col-sm-12 text-center">
-            <strong
-              >“<span class="simplified">{{ entry.simplified }}</span
-              ><span class="traditional">{{ entry.traditional }}</span
-              >” IN SONG LYRICS</strong
-            >
-          </div>
+        <div class="col-sm-12 text-center">
+          <strong
+            >“<span class="simplified">{{ entry.simplified }}</span
+            ><span class="traditional">{{ entry.traditional }}</span
+            >” IN SONG LYRICS</strong
+          >
+        </div>
       </div>
     </div>
 
-    <!-- <div class="container-flud lyrics-bar">
-      <div class="songs" v-if="!show">
-        <div class="row song">
-          <div class="container">
-            <div class="row">
-              <div class="col-md-12 text-center lyrics-wrapper">
-                <button class="btn show-more" :data-bg-hsk="entry.hsk" @click="showLRCs">Show Songs and Music Videos</button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div> -->
-
     <div class="container-flud lyrics-bar" v-cloak>
       <div class="songs">
-        <div class="row song" v-for="(lrc, lrcIndex) in lrcs.slice(0, limit)">
-          <div class="container">
-            <div class="row">
-              <div class="col-md-6 text-center lyrics-wrapper sm-mb2">
-                <PinyinButton class="mb-3" />
-                <div
-                  class="lyrics collapsed"
-                  :id="'lyrics-' + entry.id + '-' + lrcIndex"
-                  data-collapse-target
-                >
-                  <div
-                    class="lyrics-title"
-                    v-html="lrc.artist + '《' + lrc.title + '》'"
-                  ></div>
-                  <hr />
-                  <div
-                    class="lyrics-line"
-                    v-for="(line, lineIndex) in lrc.content"
-                    v-if="!LRC.rejectLine(line.line)"
-                    v-bind:class="{
-                      matched: lrc.matchedLines.includes(lineIndex),
-                      'matched-context': LRC.inContext(lineIndex, 2, lrc)
-                    }"
-                    v-on:click="seekYouTube(lrc, line.starttime)"
-                    v-html="
-                      Helper.highlight(line.line, entry.simplified, entry.hsk)
-                    "
-                  ></div>
-                </div>
-                <ShowMoreButton :data-bg-hsk="entry.hsk" class="mt-4" />
-              </div>
-              <div class="col-md-6 text-center">
-                <div
-                  class="youtube-versions"
-                  :id="`${_uid}-lrc-${lrcIndex}-youtube`"
-                >
-                  <div class="youtube" v-for="youtube in lrc.youtube">
-                    <div
-                      v-bind:style="{
-                        backgroundImage:
-                          'url(' +
-                          '//img.youtube.com/vi/' +
-                          youtube +
-                          '/hqdefault.jpg' +
-                          ')'
-                      }"
-                      class="youtube-screen"
-                      v-on:click="
-                        loadYouTubeiFrame(
-                          youtube,
-                          lrc.content[lrc.matchedLines[0]].starttime,
-                          `${_uid}-lrc-${lrcIndex}-youtube-${youtube}`,
-                          lrc
-                        )
-                      "
-                    >
-                      <div
-                        :id="`${_uid}-lrc-${lrcIndex}-youtube-${youtube}`"
-                      ></div>
-                    </div>
-                  </div>
-                  <div class="mt-4">
-                    Showing {{ lrc.currentYoutubeIndex }} of
-                    {{ lrc.youtube.length }} videos of this song.
-                    <button
-                      v-on:click="cycleYouTube(lrc, lrcIndex)"
-                      class="youtube-version-button btn-small"
-                    >
-                      Show Next
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        <Song
+          v-for="(lrc, lrcIndex) in lrcs.slice(0, limit)"
+          :entry="entry"
+          :lrc="lrc"
+          :lrcIndex="lrcIndex"
+        />
       </div>
     </div>
     <!-- .lyrics-bar -->
@@ -119,8 +35,12 @@
 import $ from 'jquery'
 import LRC from '@/lib/lrc'
 import Helper from '@/lib/helper'
+import Song from '@/components/Song'
 
 export default {
+  components: {
+    Song
+  },
   props: {
     entry: {
       type: Object
@@ -140,90 +60,9 @@ export default {
   methods: {
     showLRCs() {
       this.show = true
-    },
-    removeYouTubeAPIVars() {
-      if (window['YT']) {
-        let vars = [
-          'YT',
-          'YTConfig',
-          'onYTReady',
-          'yt',
-          'ytDomDomGetNextId',
-          'ytEventsEventsListeners',
-          'ytEventsEventsCounter'
-        ]
-        vars.forEach(function(key) {
-          window[key] = undefined
-        })
-      }
-    },
-
-    loadYouTubeiFrame(youtube, starttime, elementId, lrc) {
-      var player
-      // $('.youtube iframe').remove();
-      this.removeYouTubeAPIVars()
-      window.onYouTubePlayerAPIReady = function() {
-        // eslint-disable-next-line no-undef
-        player = new YT.Player(elementId, {
-          height: '390',
-          width: '640',
-          videoId: youtube,
-          playerVars: {
-            start: parseInt(starttime),
-            autoplay: 1,
-            controls: 1,
-            showinfo: 0,
-            playsinline: 1,
-            rel: 0
-          },
-          events: {
-            onReady: window.onPlayerReady,
-            onStateChange: function(playerStatus) {
-              if (playerStatus == 1) {
-                // Playing, update time
-                lrc.timer.setCurrentTime(lrc, player.getCurrentTime())
-                lrc.timer.play()
-              }
-              if (playerStatus == 2) {
-                // Playing, update time
-                lrc.timer.setCurrentTime(lrc, player.getCurrentTime())
-                lrc.timer.pauase()
-              }
-            }
-          }
-        })
-        lrc.youtubePlayer = player
-      }
-      $.getScript('//www.youtube.com/iframe_api')
-    },
-
-    seekYouTube(lrc, starttime) {
-      var player = lrc.youtubePlayer
-      player.seekTo(starttime)
-    },
-
-    cycleYouTube(lrc, index) {
-      var $versions = $('#lrc-' + index + '-youtube')
-      $versions.find('.youtube:first-child').appendTo($versions)
-      lrc.currentYoutubeIndex += 1
-      if (lrc.currentYoutubeIndex > lrc.youtube.length) {
-        lrc.currentYoutubeIndex = lrc.currentYoutubeIndex - lrc.youtube.length
-      }
-      var $youtube = $versions.find('.youtube:first-child .youtube-screen')
-      $youtube.click() // Load the iframe
     }
   },
   mounted() {
-    // eslint-disable-next-line no-unused-vars
-    window.onYouTubePlayerAPIReady = function() {
-      // This needs to be in global scope as YouTube api will call it
-      // This function is overwridden from the app.loadYouTubeiFrame() function
-    }
-
-    // eslint-disable-next-line no-unused-vars
-    window.onPlayerReady = function(evt) {
-      // Required by YouTube API
-    }
     LRC.getLrcs(this.entry.simplified, lrcs => {
       for (let lrc of lrcs) {
         lrc.matchedLines = []
