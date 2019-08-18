13cbe0135d2a155948647d97ca6af808ed0b7460
Jon Long <jon.long@voxelcreative.ca>
2019-08-18 17:38:51 -0400

    Home page header with Ken and I

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,47 @@
 <template>
   <div>
-    <div class="jumbotron">
-      Chinese Zero to Hero
+    <div class="jumbotron bg-white mb-0" style="overflow: hidden; position: relative">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-3"></div>
+          <div class="col-sm-12 col-md-6">
+            <YouTubeVideo youtube="5tCCX95G2vg" class="shadow" style="z-index: 2" />
+          </div>
+          <div class="col-md-3"></div>
+        </div>
+      </div>
+      <img src="img/ken-standing.png" alt="Ken" class="photo-ken d-none d-md-block">
+      <img src="img/jon-sitting.png" alt="Jon" class="photo-jon d-none d-md-block">
+    </div>
+    <div class="jumbotron bg-light mb-0">
+      <div class="container">
+
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+import YouTubeVideo from '@/components/YouTubeVideo'
+export default {
+  components: {
+    YouTubeVideo
+  }
+}
 </script>
 
-<style></style>
+<style>
+.photo-jon,
+.photo-ken {
+  position: absolute;
+  bottom: 0;
+}
+.photo-jon {
+  height: 70%;
+  right: 0;
+}
+.photo-ken {
+  height: 90%;
+  left: 0;
+}
+</style>
