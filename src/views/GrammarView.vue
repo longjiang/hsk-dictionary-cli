ba1c3545fb7769d8bdcd88867325ff8e1f907518
Jon Long <jon.long@voxelcreative.ca>
2019-08-21 12:09:55 -0700

    Grammar note styling.

--- src/views/GrammarView.vue
+++ src/views/GrammarView.vue
@@ -1,13 +1,54 @@
 <template>
   <div class="container mt-5 mb-5 main">
     <div class="row">
-      <div class="col-sm-12"></div>
+      <div class="col-sm-12 text-center">
+        <h3 class="mb-5">Grammar Note {{ grammar.code }}</h3>
+        <GrammarPoint v-if="grammar" :grammar="grammar" class="shadow rounded p-5 mb-5" />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+import Helper from '@/lib/helper'
+import GrammarPoint from '@/components/GrammarPoint'
+import ChineseZeroToHeroVue from '../ChineseZeroToHero.vue'
+
+export default {
+  components: {
+    GrammarPoint
+  },
+  props: {
+    id: {
+      type: String
+    }
+  },
+  methods: {
+    loadGrammar() {
+      $('#chinesezerotohero')[0].scrollIntoView()
+      Helper.loaded(
+        (LoadedAnnotator, LoadedHSKCEDICT, loadedGrammar, LoadedHanzi) => {
+          this.grammar = loadedGrammar._grammarData.find(
+            row => row.id === this.id
+          )
+        }
+      )
+    }
+  },
+  data() {
+    return {
+      grammar: undefined
+    }
+  },
+  mounted() {
+    this.loadGrammar()
+  },
+  watch: {
+    id() {
+      this.loadGrammar()
+    }
+  }
+}
 </script>
 
 <style></style>
