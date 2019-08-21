13f62b02724e2e813862c94119ec81e4e17ce6df
Jon Long <jon.long@voxelcreative.ca>
2019-08-21 11:46:09 -0700

    Use Helper.highlightMultiple to highlight keywords in <GrammarPoint>, so we don't need to use 'entry'

--- src/components/EntryGrammar.vue
+++ src/components/EntryGrammar.vue
@@ -6,42 +6,7 @@
     >
       <div class="label song-label mb-2">Grammar notes</div>
       <div class="grammar-notes" id="grammar">
-        <div v-for="grammar in entry.grammarPoints" class="grammar-notes-item">
-          <Annotate tag="div" class="character-example-header-word grammar-structure">
-            <b
-              v-html="
-                Helper.highlight(
-                  grammar.structure,
-                  entry.simplified,
-                  grammar.book
-                )
-              "
-            ></b>
-          </Annotate>
-          <div class="example-sentence-english grammar-english">
-            {{ grammar.english }}
-          </div>
-          <Annotate tag="div"
-            class="example-sentence-word grammar-example"
-            v-html="
-              Helper.highlight(grammar.example, entry.simplified, grammar.book)
-            "
-          ></Annotate>
-          <div class="character-example-english grammar-example-english">
-            {{ grammar.exampleTranslation }}
-          </div>
-          <a
-            v-if="grammar.url !== ''"
-            :href="grammar.url"
-            :data-bg-hsk="grammar.book"
-            class="show-more mt-3 pl-3 pr-3"
-            target="_blank"
-          >
-            <i class="glyphicon glyphicon-facetime-video mr-2"></i>
-            <b>Grammar Lesson</b>
-            {{ grammar.book }}.{{ grammar.lesson }}.{{ grammar.number }}
-          </a>
-        </div>
+        <GrammarPoint v-for="grammar in entry.grammarPoints" :grammar="grammar" />
       </div>
     </div>
   </div>
@@ -49,9 +14,13 @@
 <script>
 import Helper from '@/lib/helper'
 import Grammar from '@/lib/grammar'
+import GrammarPoint from '@/components/GrammarPoint'
 
 export default {
   props: ['entry'],
+  components: {
+    GrammarPoint,
+  },
   data() {
     return {
       Helper,
---
+++ src/components/GrammarPoint.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="grammar-notes-item">
+    <Annotate tag="div" class="character-example-header-word grammar-structure">
+      <b
+        v-html="
+          Helper.highlightMultiple(
+            grammar.structure,
+            grammar.words,
+            grammar.book
+          )
+        "
+      ></b>
+    </Annotate>
+    <div class="example-sentence-english grammar-english">
+      {{ grammar.english }}
+    </div>
+    <Annotate
+      tag="div"
+      class="example-sentence-word grammar-example"
+      v-html="
+        Helper.highlightMultiple(grammar.example, grammar.words, grammar.book)
+      "
+    ></Annotate>
+    <div class="character-example-english grammar-example-english">
+      {{ grammar.exampleTranslation }}
+    </div>
+    <a
+      v-if="grammar.url !== ''"
+      :href="grammar.url"
+      :data-bg-hsk="grammar.book"
+      class="show-more mt-3 pl-3 pr-3"
+      target="_blank"
+    >
+      <i class="glyphicon glyphicon-facetime-video mr-2"></i>
+      <b>Grammar Lesson</b>
+      {{ grammar.book }}.{{ grammar.lesson }}.{{ grammar.number }}
+    </a>
+  </div>
+</template>
+
+<script>
+import Helper from '@/lib/helper'
+export default {
+  data() {
+    return {
+      Helper
+    }
+  },
+  props: {
+    grammar: {
+      type: Object
+    }
+  }
+}
+</script>
+
+<style></style>
