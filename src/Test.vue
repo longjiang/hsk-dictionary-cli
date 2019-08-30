b8c4688f133dc9f13565afd2eea33e75c67787d1
Jon Long <jon.long@voxelcreative.ca>
2019-08-30 10:29:51 -0700

    Able to get YouTube videos of any channel

--- src/Test.vue
+++ src/Test.vue
@@ -4,7 +4,9 @@
       <div class="row">
         <div class="col-sm-12">
           <div id="annotate-me" class="show-pinyin show-simplified">
-            <div class="_2FCtq-QzlfuN-SwVMUZMM3 _2v9pwVh0VUYrmhoMv1tHPm t3_cp99mw">
+            <div
+              class="_2FCtq-QzlfuN-SwVMUZMM3 _2v9pwVh0VUYrmhoMv1tHPm t3_cp99mw"
+            >
               <div class="y8HYJ-y_lTUHkQIc1mdCq _2INHSNB8V5eaWp4P0rY_mE">
                 <div
                   class="_2SdHzo12ISmrC8H86TgSCp _29WrubtjAcKqzJSPdQqQ4h"
@@ -48,7 +50,8 @@
                       class="_3t5uN8xUmg0TOwRCOGQEcU"
                       rel="noopener noreferrer"
                       target="_blank"
-                    >https://www.youtube.com/playlist?list=PLxrrEGwTz_JoirAqymLPCMBXfsyK7iJRP</a>
+                      >https://www.youtube.com/playlist?list=PLxrrEGwTz_JoirAqymLPCMBXfsyK7iJRP</a
+                    >
                   </p>
                 </div>
               </div>
@@ -61,8 +64,7 @@
               <br />伊伏在地上；车夫便也立住脚。我料定这老女人并没有伤，又没有别人看见，便很怪他多事，要自己惹出是非，也误了我的路。
               <br />我便对他说，“没有什么的。走你的罢！”
               <br />车夫毫不理会，——或者并没有听到，——却放下车子，扶那老女人慢慢起来，搀着臂膊立定，问伊说：
-              <br />“你怎么啦？”
-              <br />“我摔坏了。”
+              <br />“你怎么啦？” <br />“我摔坏了。”
               <br />我想，我眼见你慢慢倒地，怎么会摔坏呢，装腔作势罢了，这真可憎恶。车夫多事，也正是自讨苦吃，现在你自己想法去。
               <br />车夫听了这老女人的话，却毫不踌躇，仍然搀着伊的臂膊，便一步一步的向前走。我有些诧异，忙看前面，是一所巡警分驻所，大风之后，外面也不见人。这车夫扶着那老女人，便正是向那大门走去。
               <br />我这时突然感到一种异样的感觉，觉得他满身灰尘的后影，刹时高大了，而且愈走愈大，须仰视才见。而且他对于我，渐渐的又几乎变成一种威压，甚而至于要榨出皮袍下面藏着的“小”来。
@@ -142,7 +144,10 @@ export default {
       })
     },
     testYouTube() {
-      YouTube.channelVideosByProxy('https://www.youtube.com/user/TEDxTaipei')
+      YouTube.channelVideosByProxy(
+        'https://www.youtube.com/user/TEDxTaipei',
+        youtubes => console.log(youtubes)
+      )
     }
   },
   mounted() {
--- src/lib/youtube.js
+++ src/lib/youtube.js
@@ -8,23 +8,23 @@ export default {
     if (this.limit) {
       url = url + this.limit
     }
-    $.getJSON(url, function (result) {
+    $.getJSON(url, function(result) {
       youtubeVue.lrcs = result
     })
   },
   searchYouTubeByProxy(searchTerm, callback) {
     $.ajax(
       Config.lrcServer +
-      'proxy.php?' +
-      'https://www.youtube.com/results?search_query=' +
-      searchTerm
-    ).done(function (response) {
+        'proxy.php?' +
+        'https://www.youtube.com/results?search_query=' +
+        searchTerm
+    ).done(function(response) {
       var videoIds = []
       // We use 'ownerDocument' so we don't load the images and scripts!
       // https://stackoverflow.com/questions/15113910/jquery-parse-html-without-loading-images
       var ownerDocument = document.implementation.createHTMLDocument('virtual')
       let $html = $(response, ownerDocument)
-      $html.find('.item-section li .yt-uix-tile-link').each(function () {
+      $html.find('.item-section li .yt-uix-tile-link').each(function() {
         if (
           !$(this)
             .attr('href')
@@ -44,17 +44,29 @@ export default {
       callback(videoIds)
     })
   },
-  channelVideosByProxy(channelURL, callback) { // channelURL: https://www.youtube.com/user/TEDxTaipei https://www.youtube.com/channel/UCKFB_rVEFEF3l-onQGvGx1A
-    Helper.scrape2(
-      `${channelURL}/videos`,
-      $html => {
-        for (let item of $html.find('.yt-lockup-content')) {
-          let badge = $(item).find('.yt-badge')[0]
-          if (badge && badge.innerText === 'CC') {
-            console.log($(item).find('.yt-uix-sessionlink').attr('title'))
-          }
+  channelVideosByProxy(channelURL, callback) {
+    // channelURL: https://www.youtube.com/user/TEDxTaipei https://www.youtube.com/channel/UCKFB_rVEFEF3l-onQGvGx1A
+    Helper.scrape2(`${channelURL}/videos`, $html => {
+      let youtubes = []
+      for (let item of $html.find('.yt-lockup-content')) {
+        let badge = $(item).find('.yt-badge')[0]
+        let youtube = {
+          cc: false,
+          title: $(item)
+            .find('.yt-uix-sessionlink')
+            .attr('title'),
+          url:
+            'https://www.youtube.com' +
+            $(item)
+              .find('.yt-uix-sessionlink')
+              .attr('href')
         }
+        if (badge && badge.innerText === 'CC') {
+          youtube.cc = true
+        }
+        youtubes.push(youtube)
       }
-    )
+      callback(youtubes)
+    })
   }
 }
