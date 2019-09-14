35ae8c4fcec47a638323a1775e366f475f9de81c
Jon Long <jon.long@voxelcreative.ca>
2019-09-14 07:15:02 +0200

    Add English Zero to Hero logos.

--- src/components/LanguageLogos.vue
+++ src/components/LanguageLogos.vue
@@ -1,19 +1,33 @@
 <template>
-  <div class="zerotohero">
-    <LanguageLogo
-      v-for="language in languages.filter(language => language.url && language.published === true).sort((a,b) => b.name > a.name ? -1 : 0)"
-      :language="language"
-    />
+  <div>
+    <h5 class="mb-4 text-white">Learn varous languages through English:</h5>
+    <div class="zerotohero">
+      <LanguageLogo
+        v-for="language in languages.filter(language => language.url && language.published === true).sort((a,b) => b.name > a.name ? -1 : 0)"
+        :language="language"
+      />
+    </div>
+    <hr class="border-light" style="opacity: 0.5" />
+    <h5 class="mt-4 mb-4 text-white">Learn English through various languages:</h5>
+    <div class="zerotohero">
+      <EnglishLogo
+        v-for="language in languages.filter(language => language.enURL && language.enPublished === true).sort((a,b) => b.name > a.name ? -1 : 0)"
+        class="zerotohero-item"
+        :language="language"
+      />
+    </div>
   </div>
 </template>
 
 <script>
 import LanguageLogo from '@/components/LanguageLogo'
+import EnglishLogo from '@/components/EnglishLogo'
 import Config from '@/lib/config'
 
 export default {
   components: {
-    LanguageLogo
+    LanguageLogo,
+    EnglishLogo
   },
   data() {
     return {
@@ -34,6 +48,6 @@ export default {
   justify-content: flex-start;
 }
 .zerotohero-item {
-  width: 12rem;
+  width: 15rem;
 }
 </style>
